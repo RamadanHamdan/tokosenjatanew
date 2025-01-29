8a2816b862533bcf666133886528b4c82ec6c059
Ramadan <ramadanhamdan98@gmail.com>
2025-01-29 22:37:00 +0700

    Mengubah ajax

---
+++ ajax/penjualan.php
@@ -0,0 +1,51 @@
+<?php
+sleep(1);
+require '../functions.php';
+
+$keyword = $_GET["keyword_penjualan"];
+
+$query = "SELECT * FROM penjualan 
+            WHERE 
+            nama_senjata LIKE '%$keyword%' OR
+            type_senjata LIKE '%$keyword%' OR
+            warna LIKE '%$keyword%'
+            ";
+$weapon = query($query);
+
+?>
+<table border="5" cellpadding="5" cellspacing="5">
+    <tr>
+        <th>No</th>
+        <th>Id Barang</th>
+        <th>Nama Senjata</th>
+        <th>Gambar</th>
+        <th>Type Senjata</th>
+        <th>Warna</th>
+        <th>Qty_Beli</th>
+        <th>Harga</th>
+        <th>Tanggal Update</th>
+        <th>Tanggal Input</th>
+    </tr>
+    <?php $i = 1; ?>
+    <?php foreach ($weapon as $row) : ?>
+       <tr>
+    <td><?= $i; ?></td>
+    <td>
+    <a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> |
+            <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="
+                return confirm('sure?');">hapus</a>
+            <a href="beli.php?id=<?= $row["id"];?>">beli ?</a>
+        </td>
+        <td><?= $row["id_barang"];?></td>
+        <td><?= $row["nama_senjata"];?></td>
+        <td><img src="img/<?= $row["gambar"];?>"width="100"></td>
+        <td><?= $row["type_senjata"];?></td>
+        <td><?= $row["warna"];?></td>
+        <td><?= $row["qty_beli"];?></td>
+        <td><?= $row["harga"];?></td>
+        <td><?= $row["tgl_update"];?></td>
+        <td><?= $row["tgl_input"];?></td>
+       </tr>
+    <?php $i++; ?>
+    <?php endforeach; ?>
+    </table>
