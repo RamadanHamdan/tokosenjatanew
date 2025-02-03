f0046cd642d8844ac37d1a9f8a9a37d006b2b1f6
Ramadan <ramadanhamdan98@gmail.com>
2025-02-03 21:16:56 +0700

    Mengubah login.php

--- login.php
+++ login.php
@@ -61,33 +61,48 @@ if(isset($_POST["login"]) ) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@floating-ui/core@1.6.9"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.6.13"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"></script>
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/script.js"></script>
+    <style>
+        .content {
+            position: center;
+            margin: 150px;
+            padding: 150px;
+            align-items: center;
+            justify-items: center;
+            align-content: center;
+            justify-content: center;
+        }
+    </style>
+    <title>Halaman Login</title>
 </head>
 <body>
-    <h1>Halaman Login</h1>
-
+    <div class="content">
+    <h1>Login</h1>
     <?php if(isset($error) ) :?>
         <p style="color:red; font-style:italic">username / password salah</p>
     <?php endif;?>
-    <form action="" method="post">
-        
-    <ul>
-        <li>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username">
-        </li>
-        <li>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-        </li>
-        <li>
-        <input type="checkbox" name="remember" id="remember">
-        <label for="remember">Remember Me</label>
-        </li>
-        <li>
-            <button type="submit" name="login">Login</button>
-        </li>
-    </ul>
-    </form>
+    <form class="row g-3" action="" method="post">
+  <div class="col-auto">
+    <label for="username" class="navbar-brand">Username</label>
+    <input type="text" class="form-control" id="username" name="username">
+  </div>
+  <div class="col-auto">
+    <label for="password" class="navbar-brand">Password</label>
+    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+  </div>
+  <label for="remember">Remember</label>
+    <input type="checkbox" id="remember" name="remember">
+    <div class="col-auto">
+    <button type="submit" name="login" class="btn btn-primary mb-3">Confirm identity</button>
+    </div>
+  </form> <br>
+  </div>
 </body>
 </html>
